1b20250e31d501068b08663172f851fba8cb56b0
Jonathan-Moreira <108627997+Jonathan-Moreira@users.noreply.github.com>
2022-07-19 22:48:13 -0300

    exercicio1-feito

---
+++ exercicio1/script.js
@@ -0,0 +1,12 @@
+let nome  
+let idade  
+
+console.log (typeof nome, typeof idade);
+// deu undefined no console por não ter valor atribuido
+
+nome = prompt("qual seu nome?")
+idade = prompt("qual sua idade?")
+
+
+// console.log("Olá, meu nome é" , nome, "tenho", idade , "anos.")
+
